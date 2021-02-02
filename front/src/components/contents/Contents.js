358f85cded69e8893dddc148dd2ef9fbebbc864f
KimTaewan <wwan13@naver.com>
2021-02-02 21:24:16 +0900

    [taewan] : set style per each state

--- front/src/components/contents/Contents.js
+++ front/src/components/contents/Contents.js
@@ -7,16 +7,41 @@ import AddButton from '../addButton/AddButton'
 
 function Todo() {
 
+    const datas = [
+        {
+            date: "2020.12.21",
+            contents: "파이썬 공부 하기",
+            state: "ongoing"
+        },
+        {
+            date: "2020.12.21",
+            contents: "리엑트는 진짜 죄악 이다",
+            state: "ongoing"
+        },
+        {
+            date: "2020.12.21",
+            contents: "이거 프로젝트는 언제 다 끝내지",
+            state: "complete"
+        },
+        {
+            date: "2020.12.21",
+            contents: "진짜 하기 싫다",
+            state: "complete"
+        },
+        {
+            date: "2020.12.21",
+            contents: "나 오늘 면허 받았어 ~",
+            state: "ongoing"
+        },
+    ]
+
     return (
         <div className='contents-wrapper'>
             <SortNav />
             <div className='todo-box'>
-                <TodoObject />
-                <TodoObject />
-                <TodoObject />
-                <TodoObject />
-                <TodoObject />
-                <TodoObject />
+                {datas.map(data => (
+                    <TodoObject data={data} />
+                ))}
             </div>
             <AddButton />
         </div>
--- front/src/components/todoObject/TodoObject.js
+++ front/src/components/todoObject/TodoObject.js
@@ -4,21 +4,72 @@ import { ReactComponent as CheckIcon } from '../../icons/check.svg'
 import { ReactComponent as EditIcon } from '../../icons/edit.svg'
 import { ReactComponent as TrashIcon } from '../../icons/trash.svg'
 
-function TodoObject() {
+function CompleteContents({ data }) {
 
     return (
-        <div className='object-box'>
-            <div className='object-contents'>
-                <p className='contents-date'>2020.12.21</p>
-                <p className='contents-detail'>오늘은 집에서 이것들을 할 것임.</p>
-            </div>
-            <div className='object-buttons'>
-                <a href='#'><CheckIcon fill='#414141' width='23px' height='23px' /></a>
-                <a href='#'><EditIcon fill='#414141' width='23px' height='23px' /></a>
-                <a href='#'><TrashIcon fill='#414141' width='17px' height='17px' /></a>
-            </div>
+        <div className='object-contents'>
+            <p className='contents-date complete'>{data.date}</p>
+            <p className='contents-detail complete'>{data.contents}</p>
         </div>
     );
+
+}
+
+function CompleteButtonSet() {
+
+    return (
+        <div className='object-buttons'>
+            <a href='#'><EditIcon fill='#b2b2b2' width='23px' height='23px' /></a>
+            <a href='#'><TrashIcon fill='#b2b2b2' width='17px' height='17px' /></a>
+        </div>
+    );
+
+}
+
+function OngoingContents({ data }) {
+
+    return (
+        <div className='object-contents'>
+            <p className='contents-date ongoing'>{data.date}</p>
+            <p className='contents-detail ongoing'>{data.contents}</p>
+        </div>
+    );
+
+}
+
+function OngoingButtonSet() {
+
+    return (
+        <div className='object-buttons'>
+            <a href='#'><CheckIcon fill='#414141' width='23px' height='23px' /></a>
+            <a href='#'><EditIcon fill='#414141' width='23px' height='23px' /></a>
+            <a href='#'><TrashIcon fill='#414141' width='17px' height='17px' /></a>
+        </div>
+    );
+
+}
+
+function TodoObject({ data }) {
+    const complete = (
+        <div className='object-box'>
+            <CompleteContents data={data} />
+            <CompleteButtonSet />
+        </div>
+    )
+
+    const ongoing = (
+        <div className='object-box'>
+            <OngoingContents data={data} />
+            <OngoingButtonSet />
+        </div>
+    )
+
+    if (data.state === "complete") {
+        return complete;
+    } else {
+        return ongoing;
+    }
+
 }
 
 export default TodoObject;
