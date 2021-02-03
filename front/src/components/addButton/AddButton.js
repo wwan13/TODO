7055dccbfd2e6d72bc4a54c15d184cddc5d65046
KimTaewan <wwan13@naver.com>
2021-02-03 23:04:34 +0900

    [taewan] : change button tag to link tag

--- front/src/components/addButton/AddButton.js
+++ front/src/components/addButton/AddButton.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './AddButton.css'
 
 function AddButton() {
     return (
-        <button className='add-button'>+</button>
+        <Link to="/create" className='add-button'>+</Link>
     );
 }
 
