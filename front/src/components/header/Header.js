9b494c1ca00c0e691bd88d2b0d4c799756d93b28
KimTaewan <wwan13@naver.com>
2021-02-03 14:08:42 +0900

    [taewan] : add router in header

--- front/src/components/header/Header.js
+++ front/src/components/header/Header.js
@@ -1,14 +1,46 @@
 import React from 'react'
 import './Header.css'
+import { Route, Link } from 'react-router-dom'
 
-function Header({ children }) {
+function LogInHeader() {
 
     return (
         <div className='title-wrapper'>
-            <span className='title'>Todo List</span>
+            <Link className='title'>Todo List</Link>
+            <span className='user-info'> 로그인</span>
+        </div >
+    )
+
+}
+
+function SignInHeader() {
+    return (
+        <div className='title-wrapper'>
+            <Link className='title'>Todo List</Link>
+            <span className='user-info'>회원가입</span>
+        </div >
+    )
+}
+
+function MainHeader() {
+    return (
+        <div className='title-wrapper'>
+            <Link className='title'>Todo List</Link>
             <span className='user-info'>김태완 님</span>
             <a href="#" className='logout-button'>로그아웃</a>
         </div >
+    )
+}
+
+function Header() {
+
+    return (
+        <>
+            <Route path="/" exact={true} component={MainHeader} />
+            <Route path="/create" component={MainHeader} />
+            <Route path="/login" component={LogInHeader} />
+            <Route path="/signin" component={SignInHeader} />
+        </>
     )
 }
 
