48058f1ba119d3dceae8a936f6dcb1cf68406c2a
KimTaewan <wwan13@naver.com>
2021-02-04 21:08:39 +0900

    [taewan] : database test

--- back/todo/models.py
+++ back/todo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    # user = models.ForeignKey
+    date = models.DateTimeField()
+    contents = models.TextField(max_length = 200)
