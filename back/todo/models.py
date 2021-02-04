60d9c67e8126178d27c490e11b97d17f3952a3d5
KimTaewan <wwan13@naver.com>
2021-02-04 22:44:13 +0900

    [taewan] : reset project

--- back/todo/models.py
+++ back/todo/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Todo(models.Model):
-    # user = models.ForeignKey
-    date = models.DateTimeField()
-    contents = models.TextField(max_length = 200)
--- back/todo/views.py
+++ back/todo/views.py
@@ -1,14 +1,4 @@
 from django.shortcuts import render
-# from .models import Todo
-from .models import Todo
 from datetime import datetime
 
 # Create your views here.
-
-def test(request):
-    newTodo = Todo()
-    newTodo.date = datetime.now()
-    newTodo.contents = "aaasssddd"
-    newTodo.save()
-
-    return render(request, "test.html")
