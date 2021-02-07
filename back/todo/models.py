3cf52104974d32f38e023b8bbdd0e8c7e5f92509
KimTaewan <wwan13@naver.com>
2021-02-07 23:59:57 +0900

    [taewan] : fix author field related name in Todo model

--- back/todo/models.py
+++ back/todo/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Todo(models.Model):
-    author = models.ForeignKey(User , related_name = "todo_set", on_delete = models.CASCADE)  # 작성자
+    author = models.ForeignKey(User , related_name = "author_set", on_delete = models.CASCADE)  # 작성자
     timeline =  models.DateTimeField(auto_now_add = True)   # 작성 시간
     date = models.DateField(null = False, blank = False)    # 투두 언제까지 해야 하는지
     contents = models.CharField(max_length = 300, null = False, blank = False)  # 투두 컨텐츠
