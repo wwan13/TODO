26f1489b7c8f9b8fc19ada9d30e6044b85e9289b
KimTaewan <wwan13@naver.com>
2021-02-08 23:58:49 +0900

    [taewan] : fix error and add comment

--- front/src/components/contents/Contents.js
+++ front/src/components/contents/Contents.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Route, Link } from 'react-router-dom'
 import './Contents.css'
 import SortNav from '../sortNav/SortNav'
@@ -6,9 +6,10 @@ import TodoObject from '../todoObject/TodoObject'
 import AddButton from '../addButton/AddButton'
 import InputBox from '../inputBox/InputBox'
 import SubmitButton from '../inputBox/SubmitButton'
-import getTodoData from '../../apis/getTodoData'
 
-
+/**
+ * url에 따라 메인 컨텐츠를 바꿔줌
+ */
 function Contents() {
 
     return (
@@ -21,8 +22,12 @@ function Contents() {
     );
 }
 
+/**
+ * todo list 가 나오는 컨텐츠
+ */
 function Todo() {
 
+    var [loading, setLoading] = useState(false);
     var [datas, setDatas] = useState([])
 
     fetch('http://localhost:8000/todo/')
@@ -30,6 +35,10 @@ function Todo() {
     .then(response => {
         setDatas(response)
     })
+
+    useEffect(() => {
+        return () => setLoading(false);
+    }, []);
 
     return (
         <div className='contents-wrapper'>
@@ -44,6 +53,9 @@ function Todo() {
     );
 }
 
+/**
+ * 로그인 화면
+ */
 function Login() {
     return (
         <form action="#" method="post" className="form">
@@ -55,6 +67,9 @@ function Login() {
     );
 }
 
+/**
+ * 회원가입 화면
+ */
 function Signin() {
     return (
         <form action="#" method="post" className="form">
@@ -67,6 +82,9 @@ function Signin() {
     );
 }
 
+/**
+ * todo 생성 화면
+ */
 function CreateContents() {
     return (
         <form action="#" method="post" className="form">
