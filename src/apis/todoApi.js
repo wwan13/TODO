5b4d9e0a95e03b32da8565e36d916c353fe057d9
KimTaewan <wwan13@naver.com>
2021-02-12 17:31:36 +0900

    [taewan] : change const name

--- src/apis/todoApi.js
+++ src/apis/todoApi.js
@@ -1,27 +1,27 @@
 import React from 'react'
 import axios from 'axios'
 
-const TODO_URL = 'http://localhost:8000/api/todo/'
+const TODO_API_URL = 'http://localhost:8000/api/todo/'
 
 /**
  * GET http://localhost:8000/api/todo/
  */
 export const GETTodoList = () => {
-    return axios.get(TODO_URL)
+    return axios.get(TODO_API_URL)
 }
 
 /**
  * POST http://localhost:8000/api/todo/
  */
 export const POSTTodoObject = (todoData) => {
-
+    return axios.post(TODO_API_URL)
 }
 
 /**
  * DELETE http://localhost:8000/api/todo/todoId
  */
 export const DELETETodoObject = (todoId) => {
-    return axios.delete(TODO_URL + todoId)
+    return axios.delete(TODO_API_URL + todoId)
 }
 
 /**
