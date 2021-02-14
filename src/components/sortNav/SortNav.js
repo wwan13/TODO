6ef8c61ebf84007f652da04b25c4b62c7fba8237
KimTaewan <wwan13@naver.com>
2021-02-14 23:30:00 +0900

    [taewan] : delete useless codes

--- src/components/sortNav/SortNav.js
+++ src/components/sortNav/SortNav.js
@@ -1,19 +1,10 @@
 import React from 'react'
 import './SortNav.css'
 
-import {GET_FilteredTodoList} from '../../apis/todoApi'
-
 /**
  * todo 정렬 종류에 대한 헤더
  */
 function SortNav({onClick}) {
-
-    // const filterButton = (e) => {
-    //     console.log(e.target.id)
-    //     GET_FilteredTodoList(e.target.id).then(response => {
-    //         console.log(response)
-    //     })
-    // }
 
     return (
         <nav className='nav-wrapper'>
